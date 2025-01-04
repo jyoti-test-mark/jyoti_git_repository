303bb5b372d00ea1260e164b0ed14892e949202b
jyoti-test-mark <jyotiphatangare1109@gmail.com>
2025-01-04 18:59:26 +0530

    Add files via upload

---
+++ tests_search.py
@@ -0,0 +1,21 @@
+"""
+These test case covers DuckDuckGo searches
+"""
+def test_basic_duckduckgo_search():
+  #Given DuckDuckGo home page is displayed
+  # ToDo
+
+  #When user searches for panda
+  # ToDo
+
+  #Then the search result title  caontains panda
+  #ToDo
+
+  #And the search result query is panda
+  #ToDo
+
+  #And the search result link pertain to panda
+  #ToDo
+
+  raise Exception ("Incomplete Test")
+ 
